8079e86817f89e99effd943142ff6ab62b969fd2
Amit Yadav <amit.yadav.iitr@gmail.com>
2024-11-22 11:12:38 +0000

    initial commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+help:
+	@echo "Available commands:"
+	@echo "  install      	- Install the package in active environment"
+	@echo "  single_label 	- Run single label example"
+	@echo "  multi_label  	- Run multi label example"
+	@echo "  multi_category - Run multi category example"
+	@echo "  examples       - Run examples"
+
+install:
+	python -m pip install ".[dev]"
+
+single_label:
+	python examples/single_label/main.py
+
+multi_label:
+	python examples/multi_label/main.py
+
+multi_category:
+	python examples/multi_category/main.py
+
+examples: single_label multi_label multi_category
+
+.DEFAULT_GOAL := help
---
+++ examples/multi_label/main.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from pprint import pprint
+
+import pandas as pd
+from dotenv import load_dotenv
+
+from cosmonaut import Cosmonaut
+
+load_dotenv()
+
+
+def create_prompt(inputs: pd.Series) -> str:
+    text = inputs["text"]
+    return f"Please suggest one or two gifts for the following user: {text}"
+
+
+if __name__ == "__main__":
+    inputs = pd.DataFrame(
+        {
+            "text": [
+                (
+                    "I am a 20 year old who likes to play video games."
+                    "I buy them online often with gift cards that I get on my birthday."
+                ),
+                "I dont really do online shopping but I do like cars",
+            ]
+        }
+    )
+
+    config_filepath = Path(__file__).parent / "config.yml"
+    classifier = Cosmonaut(config_filepath, create_prompt)
+    response = classifier.run(inputs)
+
+    pprint(response[classifier.config.data.result_column].tolist())
